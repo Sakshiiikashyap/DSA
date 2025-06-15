09672238e7d7e5ff34b94aee226ed68727891140
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-06-15 13:34:00 +0530

    create function.cpp

---
+++ c++/function.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printName(string name) {
+    cout << "hey " << name;
+}
+
+int main() {
+    string name;
+    cout << "Enter your name";
+    cin >> name;
+    printName(name);
+}
