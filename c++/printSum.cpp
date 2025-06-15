c375825eb40d29180e0ec4112170e10d523a0fc8
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-06-15 14:07:14 +0530

    commit changes

---
+++ c++/printSum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//TO print sum of two numbers
+void sum(int num1, int num2) {
+    int num3 = num1 + num2;   //a+b=c
+    cout << num3;
+}
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+    sum(num1, num2);
+    return 0;
+
+}
