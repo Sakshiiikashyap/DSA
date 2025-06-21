f473d61682431d859598e91225aa0798d1853055
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-06-21 15:57:52 +0530

    commit changes

--- Patterns/problem02.cpp
+++ Patterns/problem02.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print1(int n) {
+void print2(int n) {
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+        for(int j=0; j<=i; j++){
             cout << "*";
         }
         cout << endl;
     }
-
 }
-//test case to print pattern number of times.
+
 int main() {
     int t;
-    cin >> t;
+    cin >>t;
     for(int i=0; i<t; i++){
         int n;
         cin >> n;
-        print1(n);
+        print2(n);
     }
 }
