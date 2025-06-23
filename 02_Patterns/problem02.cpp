9d52d60ec3def4fdc0e276a8006a9ebb7aa0377b
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-06-23 22:39:53 +0530

    update problem02

--- 02_Patterns/problem02.cpp
+++ 02_Patterns/problem02.cpp
@@ -83,6 +83,29 @@ void print8(int n) {
         cout << endl;
     }
 }
+void print10(int n) {
+    for(int i=1; i<=2*n-1; i++){
+        int stars = i;
+        //to break the line
+        if(i>n) stars = 2*n-i; //if row exceeds n then star = 2n-i
+        for(int j=1; j<=stars; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print11(int n) {
+    int start = 1;
+    for(int i=0; i<n; i++){
+        if(i%2 == 0) start =1;
+        else start = 0;
+        for(int j=0; j<=i; j++){
+            cout << start;
+            start = 1-start;
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     int t;
@@ -90,6 +113,6 @@ int main() {
     for(int i=0; i<t; i++){
         int n;
         cin >> n;
-        print8(n);
+        print11(n);
     }
 }
