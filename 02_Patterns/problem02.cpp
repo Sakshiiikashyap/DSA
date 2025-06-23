1a4425da20e7af9917db89505841357e21d8be4e
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-06-23 20:17:57 +0530

    update problem02

--- 02_Patterns/problem02.cpp
+++ 02_Patterns/problem02.cpp
@@ -49,7 +49,23 @@ void print6(int n) {
         cout <<  endl;
     }
 }
-
+void print7(int n) {
+    for(int i=0; i<n; i++){
+        //for space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        //for star
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        //for space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     int t;
@@ -57,6 +73,6 @@ int main() {
     for(int i=0; i<t; i++){
         int n;
         cin >> n;
-        print6(n);
+        print7(n);
     }
 }
