7ea761c831fe52c9d425b226c52cb6a9b1494bc2
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-07-08 18:27:13 +0530

    update

--- 02_Patterns/patterns.cpp
+++ 02_Patterns/patterns.cpp
@@ -160,6 +160,73 @@ void print16(int n) {
         cout << endl;
     }
 }
+void print17(int n) {
+    for(int i=0; i<n; i++){
+        // space
+        for(int j=0; j<n-i-1; j++) {
+            cout << " ";
+        }
+
+        // character
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for(int j=1; j<=2*i+1; j++) {
+            cout << ch;
+            if(j <= breakpoint) ch++;
+            else ch--;
+        }
+
+        // space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print18(int n) {
+    for(int i=0; i<n; i++){
+        for(char ch='E'-i; ch<='E'; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+void print19(int n) {
+    int iniS=0;
+    for(int i=0; i<n; i++){
+        // stars
+        for(int j=1; j<=n-i; j++){
+            cout << "*";
+        }
+        // space
+        for(int j=0; j<iniS; j++){
+            cout << " ";
+        }
+        // stars
+        for(int j=1; j<=n-i; j++){
+            cout << "*";
+        }
+        iniS += 2;
+        cout << endl;
+    }
+    iniS = 2*n-2;
+    for(int i=1; i<=n; i++){
+        // stars
+        for(int j=1; j<=i; j++){
+            cout << "*";
+        }
+        // space
+        for(int j=0; j<iniS; j++){
+            cout << " ";
+        }
+        // stars
+        for(int j=1; j<=i; j++){
+            cout << "*";
+        }
+        iniS -= 2;
+        cout << endl;
+    }
+}
 
 int main(){
     int t;
@@ -167,6 +234,6 @@ int main(){
     for(int i=0; i<t; i++){
         int n;
         cin >> n;
-        print15(n);
+        print19(n);
     }
 }
