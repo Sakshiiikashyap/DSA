5dc14cda5a15ddd4afc226fb5e41dae7b1d49fc5
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-07-09 17:41:42 +0530

    update

--- 02_Patterns/patterns.cpp
+++ 02_Patterns/patterns.cpp
@@ -249,6 +249,29 @@ void print20(int n) {
         else spaces += 2;
     }
 }
+void print21(int n) {
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || j==0 || i==n-1 || j==n-1){
+                cout << "*";
+            }
+            else cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print22(int n) {
+    for(int i=0; i<2*n-1; i++){
+        for(int j=0; j<2*n-1; j++){
+            int top=i;
+            int left=j;
+            int right=(2*n-2)-j;
+            int bottom=(2*n-2)-i;
+            cout << (n- min(min(top, bottom), min(left, right))) << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     int t;
@@ -256,6 +279,6 @@ int main(){
     for(int i=0; i<t; i++){
         int n;
         cin >> n;
-        print20(n);
+        print22(n);
     }
 }
