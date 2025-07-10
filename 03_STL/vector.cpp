1a4c1dfcbdac5203023a236676391651a4e2f6bd
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-07-10 23:20:47 +0530

    update

--- 03_STL/vector.cpp
+++ 03_STL/vector.cpp
@@ -5,6 +5,12 @@ void explainVector() {
     vector<int>v;       //creates empty container
     v.push_back(1);     //{1}
     v.emplace_back(2);  // {1, 2} dynamically increases the size
+
+    //define vector of pair
+    vector<pair<int,int>> vec;
+    v.push_back({1,2});
+    v.emplace_back(1,2);
+    
 }
 int main() {
     explainVector();
