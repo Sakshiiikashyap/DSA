8e31f7a2ffd234e6be1e97d3908b94f1f98b8e28
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-07-13 20:59:44 +0530

    update pair.cpp

--- 03_STL/pair.cpp
+++ 03_STL/pair.cpp
@@ -4,17 +4,23 @@ void explainPair() {
     // to store 2 variables.
     pair <int, int> p = {1, 4};
     cout << p.first << " " << p.second;
-
+}
+void multipleVar() {
     // To store multiple variables.
     pair <int, pair <int, int>> p = {1,{3, 4}};
     cout << p.first << " " << p.second.first << " " << p.second.second;
-
+}
+    
+void storeArr() {
     // to store arr in pairs
     pair <int, int> arr[] = {{1,2}, {3,4}, {4,5}};
     cout << arr[1].second;
+
 }
+    
+
 
 int main() {
-    explainPair();
+    storeArr();
     return 0;
 }
