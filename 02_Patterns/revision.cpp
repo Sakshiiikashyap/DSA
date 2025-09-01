755b31d9b975153df8d3ea22997060ccaed50137
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-09-01 18:07:50 +0530

    update

--- 02_Patterns/revision.cpp
+++ 02_Patterns/revision.cpp
@@ -9,7 +9,6 @@ void p1(int n) {
         cout << endl;
     }
 }
-
 void p2(int n) {
     for(int i=0; i<n; i++){
         for(int j=0; j<=i; j++){
@@ -18,7 +17,6 @@ void p2(int n) {
         cout << endl;
     }
 }
-
 void p3(int n) {
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
@@ -35,8 +33,73 @@ void p4(int n) {
         cout << endl;
     }
 }
+void p5(int n) {
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void p6(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void p7(int n){
+    for(int i=1; i<=n; i++){
+        //space
+        for(int j=0; j<n-i; j++){
+            cout << " ";
+        }
+        //star
+        for(int j=0; j<2*i-1; j++){
+            cout << "*"; 
+        }
+        //space
+        for(int j=0; j<n-i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void p8(int n) {
+    for(int i=1; i<=n; i++){
+        //space
+        for(int j=0; j<i-1; j++){
+            cout << " ";
+        }
+        //star
+        for(int j=0; j<(2*n-2*i)+1; j++){
+            cout << "*";
+        }
+        //space
+        for(int j=0; j<i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void p9(int n) {
+    for(int i=1; i<=2*n-1; i++){
+        int stars = i;
+        if(i>n) stars = 2*n -i;
+        for(int j=1; j<=stars; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+
+
+
 int main() {
     int n;
     cin >> n;
-    p4(n);
+    p9(n);
 }
