d57d7f53ebda2deb187408afd99087c88a3101f3
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-09-02 00:32:11 +0530

    update

--- 02_Patterns/revision.cpp
+++ 02_Patterns/revision.cpp
@@ -151,13 +151,36 @@ void p14(int n){
     }
 }
 void p15(int n){
-    for(char i='A'; i<='A'+n; i++){
-        for(int j=1; j<i; j++){
-            cout << i << " ";
+    for(char i=0; i<n; i++){
+        char ch='A'+i;
+        for(int j=0; j<=i; j++){
+            
+            cout << ch << " ";
         }
         cout << endl;
     }
 }
+
+
+
+// void p16(int n){
+//     char ch='A';
+//     for(int i=0; i<n; i++){
+//         //space
+//         for(int j=0; j<n-i-1; j++){
+//             cout << " ";
+//         }
+//         //alphabet
+//         for(int j=0; j<2*i+1; j++){
+//             cout << ch;
+//         }
+//         //space
+//         for(int j=0; j<n-i-1; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
 
 
