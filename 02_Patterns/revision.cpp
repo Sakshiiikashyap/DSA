d6b1dae659c301c729553a4197967909cd12f92c
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-09-09 23:17:38 +0530

    update

--- 02_Patterns/revision.cpp
+++ 02_Patterns/revision.cpp
@@ -222,7 +222,6 @@ void p18(int n){
     }
     
 }
-
 void p19(int n){
     int space = 2*n-2;
     for(int i=1; i<=2*n-1; i++){
@@ -245,6 +244,16 @@ void p19(int n){
         else space +=2;
     }
 }
+
+// void p20(int n){
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(i == 0|| j == 0|| i == n-1|| j == n-1){
+//                 cout "*"
+//             }
+//         }
+//     }
+// }
 
 int main() {
     int n;
