5ae55b0c6b1a613f583e893edd07100c4238945b
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-09-14 18:19:28 +0530

    update

--- 02_Patterns/revision.cpp
+++ 02_Patterns/revision.cpp
@@ -245,16 +245,6 @@ void p19(int n){
     }
 }
 
-void p20(int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(i == 0|| j == 0|| i == n-1|| j == n-1){
-                cout "*";
-            }
-            cout << endl;
-        }
-    }
-}
 
 int main() {
     int n;
---
+++ 03_STL/revision.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void storeV() {
+    pair<int, int> saks = {1, 2};
+    cout << saks.first << " " << saks.second;
+}
+
+//to store more than one value
+void valueV() {
+    pair<int, pair<int,int>> p;
+    cout << "Enter the values";
+    cin >> p.first >> p.second.first >> p.second.second;
+    cout << p.second.first;
+}
+// to store the arr[]
+void arrV() {
+    pair<int,int> arr[] = {{1,2}, {3,4}, {5,6}};
+    
+}
+
+ int main() {
+    valueV();
+    return 0;
+ }
