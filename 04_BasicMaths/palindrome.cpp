d095904aa2700652f37df4ae8c447cab2fd65d64
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-10-22 00:12:22 +0530

    update

---
+++ 04_BasicMaths/palindrome.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//chack if num is palindrome or not
+void checkPal(int n){
+    // if(n < 0) return false;
+    int dup = n;
+    long revN = 0;
+
+    while(n > 0){
+        int ld = n % 10;
+        revN = (revN * 10) + ld;
+        n = n / 10;
+    }
+    // return dup == revN;
+    if(revN == dup) cout << "True";
+    else cout << "False";
+}
+
+int main(){
+    int n;
+    cin >> n;
+    checkPal(n);
+    return 0;
+}
