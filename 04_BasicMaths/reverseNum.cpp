c4be39f159c40dae098629c9b3418c4fbafd2e5d
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-10-21 19:36:20 +0530

    update

--- 04_BasicMaths/reverseNum.cpp
+++ 04_BasicMaths/reverseNum.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverseNum(){
-    int n;
-    cout << "Enter the num:";
-    cin >> n;
+void reverseNum(int n){
     cout << endl;
     while(n>0) {
         int lastDigit = n%10;
         cout << lastDigit << endl;
         n = n/10;
     }
-    cout << endl;
 }
 int main(){
-    reverseNum();
+    int n;
+    cout << "Enter the num:";
+    cin >> n;
+    reverseNum(n);
     return 0;
 }
