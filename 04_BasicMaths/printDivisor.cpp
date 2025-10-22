634596ce11862fba456116bf62c0223c646a9bf6
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-10-22 23:02:02 +0530

    update

--- 04_BasicMaths/printDivisor.cpp
+++ 04_BasicMaths/printDivisor.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 
 void printDivisor(int n) {
-    for(int i = 1; i<=n; i++){
+    vector<int> ls;
+    for(int i = 1; i<=sqrt(n); i++){
         if(n % i == 0){
-            cout << i << " ";
+            ls.push_back(i);
+            if(n/i != i){
+                ls.push_back(n/i);
+            }
         }
     }
+    sort(ls.begin(), ls.end());
+    for(auto it:ls) cout << it << " ";
 }
+
 int main() {
     int n;
     cin >> n;
