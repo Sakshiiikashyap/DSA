b8cb23d3bd78860cab0a19a6e648d4c61ec13963
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-10-25 23:23:12 +0530

    update

--- 05_Recursion/sum_of_n_num_parameterised.cpp
+++ 05_Recursion/sum_of_n_num_parameterised.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int sum(int n){
-    if(n==0) return 0;
-    return n + sum(n-1);
+void f(int i, int sum){
+    if(i<1) {
+        cout << sum;
+        return;
+    }
+    f(i-1, sum+i);
 }
-int main(){
+int main() {
     int n;
     cin>>n;
-    cout << sum(n);
+    f(n, 0);
     return 0;
 }
