6d3a8c691c8e750147608ecd253f094bcaa61023
sakshi kashyap <sakshiiikashyap50@gmail.com>
2025-10-31 15:47:21 +0530

    Update

--- 07_Sorting/insertion_sort.cpp
+++ 07_Sorting/insertion_sort.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-void bubbleSort(int arr[], int n){
-    for(int i=n-1; i>=1; i--){
-        for(int j=0; j<=i-1; j++){
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+void insertionSort(int arr[], int n){
+    for(int i=0; i<=n-1; i++){
+        int j = i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
         }
     }
 }
@@ -20,7 +20,7 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    bubbleSort(arr, n);
+    insertionSort(arr, n);
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
