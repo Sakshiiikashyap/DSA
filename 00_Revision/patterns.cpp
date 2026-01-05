bf34515b7699830a29c2d0be65ac61b40e7d50ae
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-01-05 22:15:31 +0530

    update pattern

--- 00_Revision/patterns.cpp
+++ 00_Revision/patterns.cpp
@@ -2,9 +2,37 @@
 using namespace std;
 
 void pattern(int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout << "*";
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout << "*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<=i; j++){
+    //         cout << "*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<(n-i); j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=(n-i+1); j++){
+            cout<<j;
         }
         cout<<endl;
     }
