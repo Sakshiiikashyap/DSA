8a3557e58e0f20a20b169ec4f71f320f480e44ee
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-01-06 13:38:09 +0530

    .

--- 00_Revision/patterns.cpp
+++ 00_Revision/patterns.cpp
@@ -66,14 +66,70 @@ void pattern(int n){
     //     cout<<endl;
     // }
 
-    for(int i=1; i<=2*n-1; i++){
-        int stars = i;
-        if(i>n) stars = 2*n-i;
-        for(int j=0; j<stars; j++){
-            cout<<"*";
+    // for(int i=1; i<=2*n-1; i++){
+    //     int stars = i;
+    //     if(i>n) stars = 2*n-i;
+    //     for(int j=0; j<stars; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int start=1;
+    // for(int i=0; i<n; i++){
+    //     if(i%2==0) start=1;
+    //     else start=0;
+    //     for(int j=0; j<=i; j++){
+    //         cout<<start;
+    //         start = 1-start;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<j;
+    //     }
+    //     for(int j=0; j<2*n-2*i; j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=i; j>=1; j--){
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+    // int num =1;
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<num<<" ";
+    //         num=num+1;
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // for(int i=0; i<n; i++){
+    //     for(char j='A'; j<='A'+i; j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=0; i<n; i++){
+    //     for(char j='A'; j<'A'+(n-i); j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    
+    for(int i=0; i<n; i++){
+        char alpha ='A'+i;
+        for(int j=0; j<=i; j++){
+            cout<<alpha<<" ";
         }
         cout<<endl;
     }
+
 }
 
 
