85a94e2164545e92d7d2d11c79512e02427b6dae
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-01-07 19:41:35 +0530

    .

--- 00_Revision/patterns.cpp
+++ 00_Revision/patterns.cpp
@@ -155,7 +155,48 @@ void pattern(int n){
     //     cout<<endl;
     // }
 
-    
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n-i; j++){
+    //         cout<<"*";
+    //     }
+    //     for(int j=0; j<2*i; j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=0; j<n-i; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<"*";
+    //     }
+    //     for(int j=0; j<2*n-2*i; j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1; j<=i; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    int space = 2*n-2;
+    for(int i=1; i<=2*n-1; i++){
+        int stars = i;
+        if(i>n) stars=2*n-i;
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        for(int j=1; j<=space; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        if(i<n) space -= 2;
+        else space += 2;
+    }
 
 }
 
