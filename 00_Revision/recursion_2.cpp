085ed3382a258dc744311d9ea9775dc056c4be41
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-01-13 15:22:31 +0530

    .

--- 00_Revision/recursion_2.cpp
+++ 00_Revision/recursion_2.cpp
@@ -19,10 +19,34 @@ int fact(int n){
     if(n==1) return 1;
     return n * fact(n-1);
 }
+//reverse an array
+void reverse(int i, int arr[], int n){
+    // if(l>=r) return;
+    // swap(arr[l], arr[r]);
+    // reverse(l+1, arr, r-1);
+    // using one pointer
+    if(i>=n/2) return;
+    swap(arr[i], arr[n-i-1]);
+    reverse(i+1, arr, n);
+}
+bool palin(int i, string &s){
+    if(i>=s.size()/2)return true;
+    if(s[i]!= s[s.size()-i-1]) return false;
+    return palin(i+1, s);
+}
 
 int main() {
-    int n;
-    cin>>n;
-    facto(n, 1);
-    
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for(int i=0; i<n; i++){
+    //     cin>>arr[i];
+    // }
+    // reverse(0, arr, n);
+    // for(int i=0; i<n; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    string s;
+    cin>>s;
+    cout<<palin(0, s);
 }
