1dbe8f2ec7bb9b0ed1493580352cb3efe3124a6b
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-06-23 22:40:55 +0530

    .

--- 08_Array/Revision/easy.cpp
+++ 08_Array/Revision/easy.cpp
@@ -12,6 +12,57 @@ void isLargest(vector<int>&arr, int n){
     cout<<largest;
 }
 
+ void isSlargest(vector<int>&arr, int n){
+    int largest = arr[0];
+    int sLargest = -1;
+    for(int i=0; i<n; i++){
+        if(arr[i]>largest){
+            sLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i]<largest && arr[i]>sLargest){
+            sLargest = arr[i];
+        }
+    }
+    cout<<sLargest;
+ }
+
+ void samllest(vector<int>&arr, int n){
+    int smallest = arr[0];
+    int sSmallest = INT_MAX;
+    for(int i=0; i<n; i++){
+        if(arr[i]<smallest){
+            sSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if(arr[i]!=smallest && arr[i]<sSmallest){
+            sSmallest = arr[i];
+        }
+    }
+    cout<<sSmallest;
+ }
+
+ void isSorted(vector<int>&arr, int n){
+    for(int i=1; i<n; i++){
+        if(arr[i]>=arr[i-1]){
+        }
+        else{
+            cout<<"false";
+            return;
+        }
+    }
+    cout<<"true";
+ }
+ void unique(vector<int>&arr, int n){
+    int i=0;
+    for(int j=0; j<n; j++){
+        if(arr[i]!=arr[j]){
+            arr[i+1] = arr[j];
+            i++;
+        }
+    }
+    cout<<i+1;
+ }
 int main(){
     int n;
     cin>>n;
@@ -19,5 +70,5 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    isLargest(arr, n);
+    unique(arr, n);
 }
