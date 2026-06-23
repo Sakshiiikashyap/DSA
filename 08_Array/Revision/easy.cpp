cb19e67551807171dc36099c111e6e3b61096288
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-06-23 18:42:54 +0530

    .

---
+++ 08_Array/Revision/easy.cpp
@@ -0,0 +1,23 @@
+//Largest element in an array
+#include <bits/stdc++.h>
+using namespace std;
+
+void isLargest(vector<int>&arr, int n){
+    int largest = arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+    cout<<largest;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    isLargest(arr, n);
+}
