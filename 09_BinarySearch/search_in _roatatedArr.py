59eb49092cac7d794c3d25b95b4f8c1b391e4001
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-07-29 18:44:23 +0530

    sol.

---
+++ 09_BinarySearch/search_in _roatatedArr.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+        low = 0
+        high = n-1
+        while low<=high:
+            mid = (low+high)//2
+            if nums[mid] == x:
+                return mid
+            if nums[low]<=nums[mid]:
+                if nums[low]<=x and x<=nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                if nums[mid]<=x and x<=nums[high]:
+                    low = mid+1
+                else:
+                    high = mid-1
+        return -1
