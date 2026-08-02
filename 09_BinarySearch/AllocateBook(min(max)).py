51dbd0822614e5cb385884c5fc907aab7ec6c3d1
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-08-02 18:08:13 +0530

    min(max)

---
+++ 09_BinarySearch/AllocateBook(min(max)).py
@@ -0,0 +1,28 @@
+from typing import List
+class Solution:
+    def countP(self, arr, walls):
+        painter = 1
+        painterWall = 0
+        for i in range(len(arr)):
+            if painterWall + arr[i] <= walls:
+                painterWall += arr[i]
+            else: 
+                painter += 1
+                painterWall = arr[i]
+        return painter
+    def minTime (self, arr, k):
+        # code here
+        n = len(arr)
+        if k>n:
+            return -1
+        low = max(arr)
+        high = sum(arr)
+        while low<=high:
+            mid = (low+high)//2
+            painter = self.countP(arr, mid)
+            if painter>k:
+                low =mid+1
+            else:
+                high = mid-1
+        return low
+        
