f09ee003b9dae377372b2450efb00ec9f6bdbeba
sakshi kashyap <sakshiiikashyap50@gmail.com>
2026-08-05 19:13:40 +0530

    sol

---
+++ 10_LinkedList/reverse-LL-II.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        for i in range(left-1):
+            prev = prev.next
+        curr = prev.next
+        reverseNode = None
+        for i in range(right-left+1):
+            nxt = curr.next
+            curr.next = reverseNode
+            reverseNode = curr
+            curr = nxt
+        leftNode = prev.next
+        prev.next = reverseNode
+        leftNode.next = curr
+        return dummy.next
+        
